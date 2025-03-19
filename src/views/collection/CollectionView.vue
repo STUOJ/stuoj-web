7f47cbe8c6cf2c2852ccb482ec9844827805502a
ArtDragonXoX <1262922587@qq.com>
2025-03-19 11:27:43 +0800

    feat(collection): 优化题单详情页面布局和功能
    - 更新页面样式：调整卡片样式，优化字体颜色和布局
    - 添加题单介绍和题目列表切换功能
    - 在页面加载时设置文档标题
    - 引入 Notebook 和 List 图标

--- src/views/collection/CollectionView.vue
+++ src/views/collection/CollectionView.vue
@@ -6,11 +6,12 @@ import ProblemList from '@/components/problem/ProblemList.vue';
 import TextView from '@/components/text/TextView.vue';
 import { formatDateTimeStr } from "@/utils/date";
 import { useRouteParams } from '@vueuse/router';
-import { Paperclip } from '@element-plus/icons-vue';
+import { Paperclip, Notebook, List } from '@element-plus/icons-vue';
 
 const { execute } = getCollectionApi();
 
-const collection = ref<CollectionInfo>();
+const activeTab = ref<'description' | 'problemList'>('description');
+const collection = ref<CollectionInfo>({} as CollectionInfo);
 const loading = ref(true);
 
 const collectionId = useRouteParams<string>("id");
@@ -24,6 +25,7 @@ const fetchData = async () => {
                 return;
             }
             collection.value = res.value;
+            document.title = `${collection.value.title} - 题单 - STUOJ`;
         });
 
     } finally {
@@ -47,34 +49,61 @@ onMounted(async () => {
         </el-card>
         <br />
         <div class="container mx-auto p-4" v-if="!loading">
-            <el-card class="mb-8">
+            <el-card class="mb-8 !border-none shadow-sm">
                 <div class="flex-row flex-between">
-                    <h1 class="text-3xl font-bold mb-4">{{ collection?.title }}</h1>
+                    <h1 class="text-3xl font-bold text-gray-800 dark:text-gray-200">
+                        {{ collection?.title }}
+                    </h1>
                     <AvatarInfo :user="collection?.user" name :size="40" />
                 </div>
-                <el-icon>
-                    <DocumentAdd />
-                </el-icon>
-                {{ formatDateTimeStr(collection?.create_time) }}
-                <el-divider direction="vertical"></el-divider>
-                <el-icon>
-                    <Edit />
-                </el-icon>
-                {{ formatDateTimeStr(collection?.update_time) }}
-                <el-divider direction="vertical"></el-divider>
-                <el-icon>
-                    <Paperclip />
-                </el-icon>
-                {{ collection?.problem_ids?.length || 0 }}题
+
+                <div class="flex items-center space-x-4 text-sm text-gray-600 dark:text-gray-400">
+                    <div class="flex items-center">
+                        <el-icon class="mr-1">
+                            <DocumentAdd />
+                        </el-icon>
+                        <span>{{ formatDateTimeStr(collection?.create_time) }}</span>
+                    </div>
+                    <el-divider direction="vertical" />
+                    <div class="flex items-center">
+                        <el-icon class="mr-1">
+                            <Edit />
+                        </el-icon>
+                        <span>{{ formatDateTimeStr(collection?.update_time) }}</span>
+                    </div>
+                    <el-divider direction="vertical" />
+                    <div class="flex items-center">
+                        <el-icon class="mr-1">
+                            <Paperclip />
+                        </el-icon>
+                        <span>{{ collection?.problem_ids?.length || 0 }}题</span>
+                    </div>
+                </div>
             </el-card>
             <br />
             <el-card>
-                <!-- TODO:实现一个二选一的checkbox，第一个是题单介绍，第二个是题单题目列表 -->
-            </el-card>
+                <el-tabs v-model="activeTab" class="mb-4">
+                    <el-tab-pane label="题单介绍" name="description">
+                        <template #label>
+                            <el-icon class="mr-1">
+                                <Notebook />
+                            </el-icon>
+                            题单介绍
+                        </template>
+                        <TextView :content="collection?.description || ''" />
+                    </el-tab-pane>
 
-            <div>
-                <!-- TODO:根据上面checkbox的选中情况，渲染不同的组件，题单介绍参考博客页面使用markdown组件渲染，题目列表参考题目列表组件 -->
-            </div>
+                    <el-tab-pane label="题目列表" name="problemList">
+                        <template #label>
+                            <el-icon class="mr-1">
+                                <List />
+                            </el-icon>
+                            题目列表
+                        </template>
+                        <ProblemList :problems="collection?.problems || []" :show-tags="true" :show-difficulty="true" />
+                    </el-tab-pane>
+                </el-tabs>
+            </el-card>
         </div>
 
         <div class="text-center py-8" v-else>
